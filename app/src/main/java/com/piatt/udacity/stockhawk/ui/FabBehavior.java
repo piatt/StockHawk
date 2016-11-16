474f1b64ae300d22a709e2d6d3b2aee6c630fd22
Ben Piatt <benp@slalom.com>
2016-11-15 19:03:43 -0600

    Added new FAB behavior, icon, colors, updated main layout

---
+++ app/src/main/java/com/piatt/udacity/stockhawk/ui/FabBehavior.java
@@ -0,0 +1,31 @@
+package com.piatt.udacity.stockhawk.ui;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class FabBehavior extends CoordinatorLayout.Behavior<FloatingActionButton>  {
+    public FabBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+        return true;
+    }
+
+    @Override
+    public void onStopNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View target) {
+        child.show();
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+        if (dyConsumed < 0 || dyConsumed > 0) {
+            child.hide();
+        }
+    }
+}
